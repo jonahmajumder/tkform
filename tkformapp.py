52d0c9159834083012e3c4eb44695e47826f8d7d
Jonah Majumder <jmajumder9@gmail.com>
2019-05-05 23:15:05 -0400

    added support for other input types with field object inputs

--- tkformapp.py
+++ tkformapp.py
@@ -1,4 +1,6 @@
-from tkinter import Tk, Label, Button, Entry, mainloop, StringVar, messagebox
+from tkinter import *
+from tkinter import messagebox # for some reason this is not included
+import time
 
 class FormApp():
     def __init__(self, *args, **kwargs):
@@ -8,8 +10,8 @@ class FormApp():
 
         pad = 10
 
-        if 'fields' in kwargs:
-            self.formfields = kwargs['fields']
+        if len(args) > 0:
+            self.formfields = args[0]
         else:
             raise(Exception('No fields given.'))
 
@@ -18,23 +20,18 @@ class FormApp():
         else:
             self.title = 'Form App'
 
-        if 'defaults' in kwargs:
-            self.defaults = kwargs['defaults']
-        else:
-            self.defaults = ['' for f in self.formfields]
-
         self.root.title(self.title)
 
-        self.titlelabel = Label(self.root, text=self.title, padx=pad, pady=pad)
+        self.titlelabel = Label(self.root, text=self.title,
+            padx=pad, pady=pad, font="-weight bold")
         self.titlelabel.grid(row=self.currentrow, column=0, columnspan=3)
         self.currentrow += 1
 
-        self.formvals = [StringVar(self.root, value=d) for d in self.defaults]
+        self.formvals = []
         for (i, field) in enumerate(self.formfields):
-            l = Label(self.root, text=(field+':'))
+            l = Label(self.root, text=(field.label+':'))
             l.grid(row=self.currentrow, column=0)
-            e = Entry(self.root, textvariable=self.formvals[i])
-            e.grid(row=self.currentrow, column=1, sticky='NSEW')
+            self.formvals.append(self.make_input(field, self.currentrow, 1))
             self.currentrow += 1
 
         self.cancelbutton = Button(self.root, text='Cancel', command=self.cancelfcn)
@@ -49,27 +46,63 @@ class FormApp():
         self.root.protocol('WM_DELETE_WINDOW', self.on_closing)
 
         # center in screen before showing window
-        self.root.withdraw()
-        self.root.update()
-        self.center_window()
+        self.root.withdraw() # hide window
+        self.root.update() # create window while hidden
+        self.configure_window() # configure window size, etc.
 
-        self.root.deiconify()
+        self.root.deiconify() # show window once ready
 
         self.root.mainloop()
 
-    def center_window(self):
+    def make_input(self, field, row, col):
+        def make_entry():
+            valvar = StringVar(self.root, value=field.default)
+            e = Entry(self.root, textvariable=valvar)
+            e.grid(row=row, column=col, sticky='NSEW')
+            return valvar # does the returned stringvar still carry the entry value?
+        def make_checkbutton():
+            valvar = StringVar(self.root, value='')
+            return valvar
+        def make_radiobutton():
+            valvar = StringVar(self.root, value=field.default)
+            f = Frame(self.root)
+            f.grid(row=row, column=col, sticky='NSEW')
+            for (i, opt) in enumerate(field.opts):
+                r = Radiobutton(f, text=opt, value=opt, variable=valvar,
+                    anchor=W)
+                r.grid(row=0, column=i, sticky='NSEW')
+                f.grid_columnconfigure(i, weight=1)
+            return valvar
+        def make_listbox():
+            valvar = StringVar(self.root, value='')
+            return valvar
+
+        fcn_dict = {
+            'entry': make_entry,
+            'radiobutton': make_radiobutton,
+            'checkbutton': make_checkbutton,
+            'listbox': make_listbox
+        }
+
+        valvar = fcn_dict[field.widgettype]()
+        return valvar
+
+
+    def configure_window(self):
+        # center window in screen
         ws = self.root.winfo_screenwidth()
         hs = self.root.winfo_screenheight()
         w = self.root.winfo_reqwidth()
         h = self.root.winfo_reqheight()
         x = ws/2 - w/2
         y = hs/3 - h/2
-
-        self.root.minsize(w, h)
         self.root.geometry('+%d+%d' % (x, y))
 
-        self.root.grid_columnconfigure(1, weight=1)
+        # do not allow window to be made any smaller
+        self.root.minsize(w, h)
 
+        # set window resizing behavior
+        self.root.grid_columnconfigure(1, weight=1)
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_rowconfigure(self.root.grid_size()[1]-1, weight=1)
 
@@ -77,8 +110,8 @@ class FormApp():
 
     def getvalues(self):
         valuedict = {}
-        for i in range(len(self.formfields)):
-            valuedict[self.formfields[i]] = self.formvals[i].get()
+        for i in range(len(self.formvals)):
+            valuedict[self.formfields[i].label] = self.formvals[i].get()
         return valuedict
 
     def cancelfcn(self):
@@ -90,23 +123,58 @@ class FormApp():
         self.root.destroy()
 
     def on_closing(self):
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        if messagebox.askokcancel('Quit', 'Are you sure you want to quit?'):
             self.values = {}
             self.root.destroy()
 
+class FormField():
+    def __init__(self, *args):
+        flderr = Exception('Invalid field definition.')
 
-def getuserformvalues(fields, **kwargs):
-    app = FormApp(fields=fields, **kwargs)
-    return app.values
+        if len(args) < 3:
+            raise(Exception('Not enough input args (expects label, widget type, default).'))
+
+        strtype = type('')
+        if type(args[0]) == strtype:
+            self.label = args[0]
+        else:
+            raise(Exception('First argument (label) must be string.'))
+
+        widgets = ['entry', 'radiobutton', 'checkbutton', 'listbox']
+        if args[1].lower() in widgets:
+            self.widgettype = args[1]
+        else:
+            raise(Exception('Invalid widget type.'))
+
+        self.default = args[2]
+
+        if self.widgettype in ['radiobutton', 'listbox']:
+            if len(args) > 3:
+                if len(args[3]) > 1:
+                    if self.default in args[3]:
+                        self.opts = args[3]
+                    else:
+                        raise(Exception('Default value must be an option.'))
+                else:
+                    raise(Exception('List of options must contain more than one item.'))
+            else:
+                raise(Exception(self.widgettype.capitalize() +
+                    ' requires fourth argument for list of options.'))
 
 if __name__ == '__main__':
 
-    fields = ['Name', 'Recipient', 'Value', 'Message']
-    defs = ['Jonah', 'My friend', '100', 'Hello!']
+    # example list of field objects
+    fields = [
+        FormField('Name', 'entry', 'Jonah'),
+        FormField('Recipient', 'entry', 'My Friend'),
+        FormField('Total cost', 'entry', '$100'),
+        FormField('Message', 'entry', 'Hi!'),
+        FormField('Action', 'radiobutton', 'Send', ['Send', 'Display', 'Nothing'])
+    ]
 
-    vals = getuserformvalues(fields, title='Test App', defaults=defs)
-    if len(vals.keys()) > 0:
-        print(vals)
+    app = FormApp(fields, title='Test App')
+    vals = app.values
+    print(vals)
 
 
 
